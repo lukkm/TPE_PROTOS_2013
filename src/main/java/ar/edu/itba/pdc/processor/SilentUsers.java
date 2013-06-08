198c8aef704bbfce0b16438f7e7fbb5ba8569c4e
Agustin Pagnoni <aguspagnoni@hotmail.com>
2013-06-08 17:22:10 -0300

    nuevo branch

---
+++ src/main/java/ar/edu/itba/pdc/processor/SilentUsers.java
@@ -0,0 +1,18 @@
+package ar.edu.itba.pdc.processor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class SilentUsers {
+	
+	/* Mapa de usuarios silenciados con formato from */
+	private Map<String,Set<String>> mapOfSilence = new HashMap<String, Set<String>>();
+	
+	public SilentUsers () {
+		
+	}
+	
+	public 
+
+}
