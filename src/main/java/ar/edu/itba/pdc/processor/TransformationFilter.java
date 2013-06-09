d44fccd22262a09e2cca605786b0fe8aed2ff7a8
Agustin Pagnoni <aguspagnoni@hotmail.com>
2013-06-09 04:24:04 -0300

    Agregado transformaciones

--- src/main/java/ar/edu/itba/pdc/processor/TransformationFilter.java
+++ src/main/java/ar/edu/itba/pdc/processor/TransformationFilter.java
@@ -1,14 +1,18 @@
 package ar.edu.itba.pdc.processor;
 
+import ar.edu.itba.pdc.jabber.Message;
 import ar.edu.itba.pdc.stanzas.Stanza;
 
 public class TransformationFilter implements Filter{
 	
-	char[] changedVocabulary = {};
+	char[] changedVocabulary = "4B<D3FGH1JKLMN0PQRSTUVWQYZ4b<d3fgh1jklmn0pqrstuvwqyz".toCharArray();
 	
 	public void apply(Stanza stanza) {
+		String transformedMessage = "";
 		if (stanza.isMessage()) {
-			
+			for (char c : ((Message)stanza.getElement()).getMessage().toCharArray())
+				transformedMessage += changedVocabulary[c - 'A'];
+			((Message)stanza.getElement()).setMessage(transformedMessage);
 		}
 	}
 
